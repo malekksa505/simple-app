5777c85ffce91c66aec23e8999f7db17e01d9645
MaLeK Darwish <malekksa505@gmail.com>
2024-06-24 04:00:35 +0300

    frontend

--- malek-trpc-prisma/app/page.tsx
+++ malek-trpc-prisma/app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
 import { trpc } from "@/server/client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  const getUsers = trpc.user.getUsers.useQuery()
+  const getUsers = trpc.user.getUsers.useQuery();
+  const addUsers = trpc.user.addUsers.useMutation();
+
+  const [name, setName] = useState<string>("")
+  const [gender, setGender] = useState<string>("")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {JSON.stringify(getUsers.data)}
+      <div>
+        Name:{""}
+        <input value={name} onChange={(e) =>setName(e.target.value)} type="text" />
+        Gender:{""}
+        <input value={gender} onChange={(e) =>setGender(e.target.value)} type="text" />
+        <button onClick={() => addUsers.mutate({name,gender})}>
+            Send
+        </button>
+      </div>
     </main>
   );
 }
--- malek-trpc-prisma/server/routers/user.ts
+++ malek-trpc-prisma/server/routers/user.ts
@@ -1,15 +1,22 @@
 import { z } from "zod";
 import { procedure, router } from "../trpc";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient();
 
 export const userRouter = router({
-    getUsers: procedure.query(() =>{
-        return Prisma
+    getUsers: procedure.query(async() =>{
+        return await Prisma.user.findMany()
     }),
     addUser: procedure.input(z.object({
         name:z.string(),
         gender:z.string()
-    })).mutation((opts) => {
+    })).mutation(async (opts) => {
         const {input} = opts;
-        // prisma
+        await prisma.user.create({
+            data: {
+                name: input.name,
+                gender: input.gender
+            }
+        })
     })
 })
